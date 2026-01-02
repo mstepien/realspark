1131348b0694e5259d837253e0eee26881ffa246
Marcin Stepien <marcin.stepien@smart.biz.pl>
2026-01-02 17:20:55 +0100

    RGB histogram with cv2

--- tests/analysis/test_analysis.py
+++ tests/analysis/test_analysis.py
@@ -42,25 +42,3 @@ def test_analyze_image_valid():
     assert isinstance(stats['ai_probability'], float)
     assert stats['ai_probability'] == 0.1
 
-def test_analyze_image_gradient():
-    # Create a 2x2 image
-    # (0,0): Black (0,0,0)
-    # (0,1): White (255,255,255)
-    # (1,0): White (255,255,255)
-    # (1,1): Black (0,0,0)
-    # Mean should be 127.5
-    
-    data = np.zeros((2, 2, 3), dtype=np.uint8)
-    data[0, 1] = [255, 255, 255]
-    data[1, 0] = [255, 255, 255]
-    
-    image = Image.fromarray(data)
-    img_byte_arr = io.BytesIO()
-    image.save(img_byte_arr, format='PNG')
-    img_bytes = img_byte_arr.getvalue()
-    
-    stats = analyze_image(img_bytes)
-    
-    assert stats['width'] == 2
-    assert stats['height'] == 2
-    assert stats['mean_color'] == [127.5, 127.5, 127.5]
